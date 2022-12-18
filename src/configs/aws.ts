06dceb63fad7650602707620e66cc79104e07762
maithevien9 <61836529+maithevien9@users.noreply.github.com>
2022-12-18 20:26:39 +0700

    Update aws.ts

--- src/configs/aws.ts
+++ src/configs/aws.ts
@@ -1,6 +1,6 @@
 export const s3Config = {
-  accessKeyId: 'AKIA2PCTTTFNC3IFFHTH',
+  accessKeyId: '',
   bucketName: 'nft-web',
   region: 'ap-northeast-1',
-  secretAccessKey: '79I/18ADhxbTsqLpH+2kSX+bGmXxZ+Q0Taj9XqQi',
+  secretAccessKey: '',
 };
